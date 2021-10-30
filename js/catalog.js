1d32dc7a54527ba024e16531c949cf1c44644e82
Ruslan Ramazanov <tiamin1989@yandex.ru>
2021-10-30 17:49:21 +0500

    add: catalog popup

---
+++ js/catalog.js
@@ -0,0 +1,30 @@
+const classes = {
+  item_smartphones: "block_smartphones",
+  item_notebooks: "block_notebooks",
+  item_tvs: "block_tvs",
+  item_audio: "block_audio",
+  item_photos: "block_photos",
+  item_appliances: "block_appliances",
+};
+
+const catalogButton = document.querySelector(".header__catalog");
+const close = document.querySelector(".catalog__close");
+const catalog = document.querySelector(".catalog");
+
+close.addEventListener("click", () => {
+  catalog.style.display = "none";
+});
+
+catalogButton.addEventListener("click", () => {
+  catalog.style.display = "flex";
+});
+
+for (let item in classes) {
+  document.querySelector(`.${item}`).addEventListener("mouseenter", (evt) => {
+    for (let item in classes) {
+      if (!evt.target.classList.contains(`${item}`))
+        document.querySelector(`.${classes[item]}`).style.display = "none";
+    }
+    document.querySelector(`.${classes[item]}`).style.display = "flex";
+  });
+}
