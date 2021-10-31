aa1b18c33761d5b47b4f3841f939b1c734f455cc
Ruslan Ramazanov <tiamin1989@yandex.ru>
2021-10-31 14:16:22 +0500

    change: catalog popup menu backlight on click

--- js/catalog.js
+++ js/catalog.js
@@ -10,6 +10,19 @@ const classes = {
 const catalogButton = document.querySelector(".header__catalog");
 const close = document.querySelector(".catalog__close");
 const catalog = document.querySelector(".catalog");
+const catItem = document.querySelectorAll(".catalog__list-item");
+
+catItem.forEach((e, i, a) => {
+  function toggleOtherEls() {
+    a.forEach((e) => {
+      e.classList.remove("catalog__list-item_active");
+    });
+  }
+  e.addEventListener("click", (evt) => {
+    toggleOtherEls();
+    evt.target.classList.toggle("catalog__list-item_active");
+  });
+});
 
 close.addEventListener("click", () => {
   catalog.style.display = "none";
@@ -20,7 +33,7 @@ catalogButton.addEventListener("click", () => {
 });
 
 for (let item in classes) {
-  document.querySelector(`.${item}`).addEventListener("mouseenter", (evt) => {
+  document.querySelector(`.${item}`).addEventListener("click", (evt) => {
     for (let item in classes) {
       if (!evt.target.classList.contains(`${item}`))
         document.querySelector(`.${classes[item]}`).style.display = "none";
