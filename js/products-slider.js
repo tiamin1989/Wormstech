822888bd4b40969377b68654e7279a8b6c4b4e78
Ruslan Ramazanov <tiamin1989@yandex.ru>
2021-11-02 13:48:31 +0500

    change: products slider

---
+++ js/products-slider.js
@@ -0,0 +1,28 @@
+const itemLength = document.querySelector(".products__list-item").offsetWidth;
+const items = document.querySelectorAll(".products__list-item");
+const list = document.querySelector(".products__list");
+const left = document.querySelector(".products__list-prev");
+const right = document.querySelector(".products__list-next");
+let shiftDistance = 0;
+let gap = parseInt(window.getComputedStyle(list).gap);
+
+function toLeft() {
+  if (shiftDistance === 0) return;
+  shiftDistance = shiftDistance + (itemLength + gap);
+  items.forEach((e) => {
+    e.style.transform = `translate(${shiftDistance}px)`;
+  });
+}
+
+function toRight() {
+  let x = (itemLength + gap) * (items.length - 1);
+  x = ~x + 1;
+  if (shiftDistance === x) return;
+  shiftDistance = shiftDistance - (itemLength + gap);
+  items.forEach((e) => {
+    e.style.transform = `translate(${shiftDistance}px)`;
+  });
+}
+
+left.addEventListener("click", toLeft);
+right.addEventListener("click", toRight);
