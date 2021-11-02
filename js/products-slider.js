14077872552d3556f8217412b751d9237051e3fa
Ruslan Ramazanov <tiamin1989@yandex.ru>
2021-11-02 15:32:34 +0500

    fixes

--- js/products-slider.js
+++ js/products-slider.js
@@ -1,10 +1,11 @@
-const itemLength = document.querySelector(".products__list-item").offsetWidth;
 const items = document.querySelectorAll(".products__list-item");
 const list = document.querySelector(".products__list");
 const left = document.querySelector(".products__list-prev");
 const right = document.querySelector(".products__list-next");
+
 let shiftDistance = 0;
 let gap = parseInt(window.getComputedStyle(list).gap);
+let itemLength = document.querySelector(".products__list-item").offsetWidth;
 
 function toLeft() {
   if (shiftDistance === 0) return;
@@ -26,3 +27,12 @@ function toRight() {
 
 left.addEventListener("click", toLeft);
 right.addEventListener("click", toRight);
+
+window.addEventListener(
+  "resize",
+  () => {
+    gap = parseInt(window.getComputedStyle(list).gap);
+    itemLength = document.querySelector(".products__list-item").offsetWidth;
+  },
+  true
+);
